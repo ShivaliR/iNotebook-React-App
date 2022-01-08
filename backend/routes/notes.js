cd5da5793547c4567d0cc0f556f9c9c780ce48fa
ShivaliR <shivaliranka12@gmail.com>
2022-01-08 13:48:26 +0530

    changes made on 07 january 2022

--- backend/routes/notes.js
+++ backend/routes/notes.js
@@ -44,4 +44,25 @@ router.post('/addNote', fetchuser, [
   }
 );
 
+//ROUTE 3: update existing notes : PUT "/api/notes/updateNote".Login required 
+router.put('/updateNote/:id', fetchuser, async (req, res)=>{
+    //using destructuring 
+    const {title, description, tag} = req.body;
+    //create a newNote object
+    const newNote = {}
+    if(title){newNote.title = title};
+    if(description){newNote.description = description};
+    if(tag){newNote.tag = tag};
+    
+    //Find the note to be updated and update it
+    let note = await Note.findById(req.params.id);
+    if(!note){return res.status(404).send("Not found!")}
+    if(note.user.toString() !== req.user.id){
+      console.log("Hi I'm inside local func" + note.user.toString()+ "&" + req.user.id )
+      return res.status(401).send("Not authorized!")
+    }
+
+    note = await Note.findByIdAndUpdate(req.params.id, {$set: newNote}, {new: true} )
+    res.json({note});
+})
 module.exports = router;
