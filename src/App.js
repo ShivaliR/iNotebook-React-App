03bf1b4c7273ee5ee5ba2763b39170f66c8babed
ShivaliR <shivaliranka12@gmail.com>
2022-01-10 12:21:04 +0530

    Changes made on 09 jan 2022

--- src/App.js
+++ src/App.js
@@ -1,9 +1,21 @@
-import './App.css';
+import "./App.css";
+import React from "react";
+import Home from "./components/Home";
+import Navbar from "./components/Navbar";
+import About from "./components/About";
+import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
 
 function App() {
   return (
-    <div className="App">
-    </div>
+    <>
+      <Router>
+        <Navbar />
+        <Routes>
+          <Route exact path="/about" element={<About />} />
+          <Route exact path="/" element={<Home />} />
+        </Routes>
+      </Router>
+    </>
   );
 }
 
---
+++ src/components/About.js
@@ -0,0 +1,9 @@
+import React from 'react'
+
+const About = () => {
+    return (
+     <h1>Hi, this is home!</h1>
+    )
+}
+
+export default About;
